575870d21f94471e2989c3874878097078cb9af2
Alex123310 <alessandrett@gmail.com>
2023-04-25 12:32:14 +0200

    changed file

--- Carica_Record_F.c
+++ Carica_Record_F.c
@@ -2,21 +2,23 @@
 calciatore Carica_Record_F ()
 {
 // caricamento e controllo campi del record
+calciatore c;
+
 do
 {
- printf ("Cognome: ");
- fflush(stdin);
- gets (c.Cognome); 
+    printf ("Cognome: ");
+    fflush(stdin);
+    gets (c.Cognome); 
 }
 while ( (strlen(c.Nome) == 0) || (strlen(c.Nome) > MAXDIM) );
 do
 {
- printf ("Nome: ");
- gets (c.Nome); 
+    printf ("Nome: ");
+    gets (c.Nome); 
 }
 while ( (strlen(c.Nome) == 0) || (strlen(c.Nome) > MAXDIM) );
- printf ("Maglia: ");
- scanf ("%d", &(c.Maglia));
+    printf ("Maglia: ");
+    scanf ("%d", &(c.Maglia));
 return (c);
 }
 
